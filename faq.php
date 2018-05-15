562b3e1e96b8649836c9cdf15a85eead72ad4a56
Romina Celeste Alvarez <rominaalvarez@MacBook-Air-de-Romina.local>
2018-05-15 17:10:37 -0300

    Primeras paginas

---
+++ faq.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Group Trip</title>
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titan+One" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <section class="left">
+        <header>
+          <div class="header-class">
+            <h1><img src="img/avion1-50.jpg">    Group Trip</h1>
+          </div>
+          <nav class="nav">
+            <ul>
+              <li>Quienes Somos</li>
+              <li>Proximos Viajes</li>
+              <li>Contacto</li>
+              <li>F.A.Q.</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section class="content">
+            <h3 class="conectate">Preguntas frecuentes</h3>
+          </section>
+          </section>
+        </main>
+      </section>
+      <aside class="right">
+      </aside>
+    </div>
+  </body>
+
+</html>
